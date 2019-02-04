8aaae8e883e422483de82cb4cf30056204418e4c
freeman-as <freeman.by2018@gmail.com>
2019-02-04 12:22:02 +0900

    [Add] ファイル分割.

---
+++ app.js
@@ -0,0 +1,29 @@
+// ロードされ、Vueがグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+console.dir(Vue);
+
+var items = [
+    {
+        name: '鉛筆',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: 'ノート',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: '消しゴム',
+        price: 500,
+        quantity: 0
+    }
+]
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    }
+})
+window.vm = vm;
