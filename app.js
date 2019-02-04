f3607c1ba4cde4b3441702ec7a09c1bbc6a99666
freeman-as <freeman.by2018@gmail.com>
2019-02-04 17:16:55 +0900

    [update] methods追加.

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ var vm = new Vue({
             return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,' );
         }
     },
+    methods: {
+        doBuy: function (event) {
+            alert(this.totalPriceWithTax + '円のお買い上げ！');
+            this.items.forEach(function (item) {
+                item.quantity = 0;
+            });
+        }
+    },
     computed: {
         totalPrice: function () {
             return this.items.reduce(function (sum, item) {
